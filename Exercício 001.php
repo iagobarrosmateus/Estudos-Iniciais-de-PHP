5383f21275b5dad6f87ed2df7114fecc4a7271e2
Iago Barros Mateus <iagomateus.9@gmail.com>
2022-09-18 21:16:09 -0300

    Exercício 001

---
+++ Exercício 001.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo.css">
+    <title>Variaveis</title>
+</head>   
+<body>
+    <header>
+        <h1>Variáveis</h1>
+    </header>
+      <div>
+        <?php
+        $idade= 30;
+        $idade1= 29;
+        $nome= "Iago";
+        $nome1 = "Igor";
+        echo $nome." Tem ".$idade. " anos ";
+        echo "$nome1 tem $idade1 anos"
+        ?>
+    </div>
+    <footer>
+        <p>&copy; Iago Barros Mateus</p>
+    </footer>
+</body>
+</html>
+
+
+
+
