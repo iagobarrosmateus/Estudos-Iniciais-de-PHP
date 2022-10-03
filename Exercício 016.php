18b2c7873a8fad449f7247a6d5f88f16840b52bc
Iago Barros Mateus <iagomateus.9@gmail.com>
2022-10-02 22:54:51 -0300

    Funções

---
+++ Exercício 016.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo.css">
+    <title>Funções</title>
+</head>
+<body>
+    <div>
+        <?php
+        //include ou require
+            include "Funcoes.php";
+            echo "<h1>Testando novas funções</h1>";
+            ola();
+            mostravalor(4);
+        ?>
+    </div>
+</body>
+</html>
