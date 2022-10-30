84fc841938170e5f926941b22062f9d10a8447ad
Iago Barros Mateus <iagomateus.9@gmail.com>
2022-10-29 21:44:32 -0300

    Lutador

---
+++ Exercício 028.php
@@ -0,0 +1,26 @@
+<?php
+class luta{
+
+private $desafiado;
+private $desafiante;
+private $rounds;
+private $aprovada;
+
+function marcarLuta(){
+
+}
+
+function lutar(){
+
+}
+
+function setDesafiado(){
+    
+}
+
+function getDesafiado(){
+
+}
+
+}
+?>
